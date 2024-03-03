9856784fc5c5ea917ff2d22599b605e3b0156349
ringoebenezer <ringo.ebenezer@gmail.com>
2024-03-03 22:48:32 +0300

    commits

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>Hello world, made some changes</Text>
       <StatusBar style="auto" />
     </View>
   );
